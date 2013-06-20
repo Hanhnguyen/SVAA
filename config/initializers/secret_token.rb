aa7c4bbc9a27604f9d653b21c0e5f7a715963572
Hanh Nguyen <hanhn13@stanford.edu>
2013-06-20 11:36:45 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SVAA::Application.config.secret_token = '77a42af6831c5d18a8f3b32697f63b9cf5d1520ca4e1dd1d49900cede95897bf35a470ecebe7c57e7c9182018324316cee3e58d493f3f611bf75c844fbf7036c'
